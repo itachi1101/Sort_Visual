003528f2f430dba406376483779e4e1a21f97f1c
Aviral <aviral1101@gmail.com>
2021-06-22 16:58:24 +0530

    disable event added

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 const arraySpace = document.querySelector(".array_space");
-
+const generatorButton = document.getElementById("generator");
+generatorButton.addEventListener("click", () => {
+  generate();
+});
 function arrayGenerator() {
-    const number = 20;
+  const number = 20;
 
-    for (let i = 0; i < number; i+=1){
-        const height = Math.floor(Math.random() * 100) + 4;
+  for (let i = 0; i < number; i += 1) {
+    const height = Math.floor(Math.random() * 100) + 4;
 
-        const verticalBar = document.createElement("div");
-        verticalBar.classList.add("bar");
+    const verticalBar = document.createElement("div");
+    verticalBar.classList.add("bar");
 
-        verticalBar.style.height = `${height * 3.5}px`;
+    verticalBar.style.height = `${height * 3.5}px`;
 
-        verticalBar.style.transform = `translateX(${i * 31}px)`;
+    verticalBar.style.transform = `translateX(${i * 31}px)`;
 
-        const BarMarkings = document.createElement("label");
-        BarMarkings.classList.add("markings");
-        BarMarkings.innerHTML = height;
+    const BarMarkings = document.createElement("label");
+    BarMarkings.classList.add("markings");
+    BarMarkings.innerHTML = height;
 
-
-        verticalBar.appendChild(BarMarkings);
-        arraySpace.appendChild(verticalBar);
-
-
-    }
-    
+    verticalBar.appendChild(BarMarkings);
+    arraySpace.appendChild(verticalBar);
+  }
 }
 arrayGenerator();
+function generate() {
+  window.location.reload();
+}
---
+++ merge.js
@@ -0,0 +1,10 @@
+const merge = document.getElementById("two");
+merge.addEventListener('click', () => {
+    mergeSort();
+    document.getElementById("two").style.pointerEvents = "none";
+    document.getElementById("generator").style.pointerEvents = "none";
+});
+
+function mergeSort() {
+    
+}
